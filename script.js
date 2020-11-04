f9932833c9b26d6e2c45b5d99ed4979d9daf2227
Sebastian-Perz <esdo.perz@gmail.com>
2020-11-04 22:35:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let button = document.querySelector("#submit")
+let input = document.querySelector("#search")
+let output = document.querySelector("#output")
+
+button.addEventListener('click', (e) => {
+    getMyLovelyDataFromItunes()
+})
+
+function getMyLovelyDataFromItunes(){
+    let url = 'https://itunes.apple.com/search?term='+input.value
+    let cors = 'https://white-snow-b130.esdo-perz.workers.dev/corsproxy/?apiurl='
+    fetch(cors+url)
+    .then(data => data.json())
+    .then(json => {
+        console.log(json)
+        let finalHTML = ''
+
+        json.results.forEach(song => {
+            finalHTML += 
+            `
+            <div class="col s4 m4 l4">
+            <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="${song.artworkUrl100}">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">${song.trackCensoredName}<i class="material-icons right">more_vert</i></span>
+                <p>${song.artistName}</p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+            </div>
+            </div>
+            `
+        })
+        output.innerHTML = finalHTML
+    })
+    .catch(error => console.log(error))
+
+}
